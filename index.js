30a47dff2bbe1977bc7c324a9248f191a29e0e12
Justin <parbaked@gmail.com>
2020-04-27 17:40:37 -0700

    full commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 for (i = 0; i < document.querySelectorAll(".drum").length; i++){
 
-
 document.querySelectorAll('button')[i].addEventListener("click", handleClick);
 
+let tom1 = new Audio("sounds/tom-1.mp3");
+
 function handleClick() {
-  alert("I got Clicked");
+tom1.play()
 }
 }
