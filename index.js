30b692a2b04bf51cb5c11a47289eb509217bad29
Justin <parbaked@gmail.com>
2020-04-27 17:14:21 -0700

    initial commit for Udemy drum kit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+for (i = 0; i < document.querySelectorAll(".drum").length; i++){
+
+
+document.querySelectorAll('button')[i].addEventListener("click", handleClick);
+
+function handleClick() {
+  alert("I got Clicked");
+}
+}
